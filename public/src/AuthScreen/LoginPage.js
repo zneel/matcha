4ceeb82facb8a47aa1a275040447ba4770c7664d
Radaxx <alexis.paraboschi@gmail.com>
2019-02-05 19:48:01 +0100

    initial commit

---
+++ public/src/AuthScreen/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoginPage.css';
+
+export default class LoginPage extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div>Toto</div>
+      </div>
+    );
+  }
+}
