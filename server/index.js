5d0397a712c33e0029ceab8e414bdd2565b2c58b
Elliot Bouvier <elliotbouvier@protonmail.com>
2019-03-21 10:16:20 +0100

    add docker-compose for mariadb and dotenv

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello from matcha");
 });
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
-});
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Example app listening on port 3000!")
+);
