427f39d0f87aa75da5b03f519594c0cfd3981598
Elliot Bouvier <elliotbouvier@protonmail.com>
2019-03-28 15:41:11 +0100

    seed

--- server/seed.js
+++ server/seed.js
@@ -1,69 +1,110 @@
 "use strict";
-const faker = require("faker");
-const mysql = require("mysql");
 require("dotenv").config();
-const connection = mysql.createPool({
-  connectionLimit: 10,
+const { Pool } = require("pg");
+const https = require("https");
+const config = {
   host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-const createUser = (con, id) => {
-  return new Promise((resolve, reject) => {
-    con.query(
-      `INSERT INTO users (id, email, username, name, surname, password, bio, genres_id) VALUES (?,?,?,?,?,?,?,?)`,
-      [
-        id,
-        faker.internet.email(),
-        faker.internet.userName(),
-        faker.name.firstName(),
-        faker.name.lastName(),
-        faker.internet.password(),
-        faker.lorem.paragraph(),
-        faker.random.number({ min: 1, max: 2 })
-      ],
-      (err, res) => {
-        if (err) return reject(err);
-        return resolve(res);
-      }
-    );
-  });
+  port: process.env.DB_PORT,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 };
+const pool = new Pool(config);
 
-const createImage = (con, is_profile) => {
-  return new Promise((resolve, reject) => {
-    con.query(
-      `INSERT INTO images (path, is_profile) VALUES (?,?)`,
-      [faker.image.avatar(), faker.random.number({ min: 0, max: 1 })],
-      (err, res) => {
-        if (err) return reject(err);
-        return resolve(res);
-      }
-    );
-  });
-};
-
-const seed = num => {
-  let users = [];
-  let images = [];
-  for (let i = 0; i <= num; i++) {
-    users.push(createUser(connection, i));
+const unseed = async pool => {
+  try {
+    let numUser = await getNumUsers(pool);
+    for (let i = 0; i <= numUser.rows[0].count; i++) {
+      await delUser(pool, i);
+      await delImage(pool, i);
+    }
+  } catch (e) {
+    console.error(e);
   }
-  for (let j = 0; j <= num * 2; j++) {
-    let is_profile = j % 5 === 0 ? 1 : 0;
-    images.push(createImage(connection, is_profile));
-  }
-  return Promise.all([...users, ...images]);
 };
 
-seed(1000)
-  .then(d => {
-    console.log(d);
-    return process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    return process.exit(1);
+const seed = async (num, pool) => {
+  const url = `https://randomuser.me/api/?results=${num}&nat=us,dk,fr,gb`;
+  https.get(url, res => {
+    res.setEncoding("utf8");
+    let body = "";
+    res.on("data", data => {
+      body += data;
+    });
+    res.on("end", () => {
+      body = JSON.parse(body);
+      body.results.forEach(async (e, i) => {
+        console.log(e);
+        const { first, last } = e.name;
+        const { city, coordinates } = e.location;
+        const { gender, email, nat } = e;
+        const { username, sha256 } = e.login;
+        const { date } = e.dob;
+        const picture = e.picture.large;
+        const user = {
+          id: i,
+          email,
+          username,
+          first,
+          last,
+          sha256,
+          gender,
+          city,
+          nat,
+          coordinates,
+          date
+        };
+        try {
+          await createUser(pool, user);
+          await createImage(pool, picture, i);
+        } catch (e) {
+          console.error(e);
+        }
+      });
+    });
   });
+};
+
+const createUser = (pool, user) => {
+  const text = `INSERT INTO matcha.user
+  (id, email, username, firstname, lastname, password, genre, city, country, location, dob) 
+  VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)`;
+  const values = [
+    user.id,
+    user.email + user.id,
+    user.username + user.id,
+    user.first,
+    user.last,
+    user.sha256,
+    user.gender,
+    user.city,
+    user.nat,
+    `(${user.coordinates.latitude},${user.coordinates.longitude})`,
+    user.date
+  ];
+  return pool.query(text, values);
+};
+
+const getNumUsers = pool => pool.query("SELECT COUNT(*) FROM matcha.user");
+const delUser = (pool, id) =>
+  pool.query("DELETE FROM matcha.user WHERE id = $1", [id]);
+const delImage = (pool, id) =>
+  pool.query("DELETE FROM matcha.image WHERE id = $1", [id]);
+const createImage = (pool, picture, id) => {
+  return pool.query(
+    `INSERT INTO matcha.image (id, path, is_profile, user_id) VALUES ($1,$2,$3,$4)`,
+    [id, picture, true, id]
+  );
+};
+
+(async () => {
+  try {
+    await unseed(pool);
+    await seed(1000, pool);
+  } catch (e) {
+    return console.error(e);
+  }
+})();
--- server/services/db/index.js
+++ server/services/db/index.js
@@ -1,23 +1,13 @@
 require("dotenv").config();
-const mysql = require("mysql");
+const { Pool } = require("pg");
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
+});
 
-class Database {
-  constructor() {
-    this.pool = mysql.createPool({
-      connectionLimit: 10,
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    });
-  }
-  query(sql, args) {
-    return new Promise((resolve, reject) => {
-      this.pool.query(sql, args, (err, rows) => {
-        if (err) return reject(err);
-        resolve(rows);
-      });
-    });
-  }
-}
-module.exports = Database;
+module.exports = pool;
